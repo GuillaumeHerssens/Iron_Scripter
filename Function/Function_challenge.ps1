f84cab8308514f39d7afcb2340680cde62a3fe47
GuillaumeHerssens <Guillaume.Herssens@gmail.com>
2021-09-28 10:33:30 +0200

    Added function challenge

---
+++ Function/Function_challenge.ps1
@@ -0,0 +1,46 @@
+
+function ToCelsius{
+    [CmdletBinding()]
+    param(
+        [parameter(ValueFromPipeline)]
+        [ValidateNotNullOrEmpty()]
+        [double]$temp
+    )
+    process{
+        $converted = [PSCustomObject]@{
+        Fahrenheit = $temp
+        Celsius = (($temp - 32) / 1.8)
+        }
+        Write-Output $converted
+    }
+
+
+}
+
+
+
+function ToFahrenheit{
+    [CmdletBinding()]
+    param(
+        [parameter(ValueFromPipeline)]
+        [ValidateNotNullOrEmpty()]
+        [double]$temp
+    )
+    process{
+        $converted = [PSCustomObject]@{
+            Celsius = $temp
+            Fahrenheit = (($temp * 1.8) + 32)
+            }
+        Write-Output $converted
+    }
+
+
+
+
+
+}
+
+$far = 32
+$far | ToCelsius
+$cel = 100
+$cel | ToFahrenheit
