585179bd999380653146ca86da06041510cda6a5
GuillaumeHerssens <Guillaume.Herssens@gmail.com>
2021-09-29 17:16:24 +0200

    Added Back to school challenge

---
+++ Back_to_school/README.md
@@ -0,0 +1,23 @@
+https://ironscripter.us/powershell-back-to-school-scripting-challenge/
+
+# PowerShell Back-to-School Scripting Challenge
+
+For many people, it is back-to-school time. This time of years always makes The Chairman nostalgic, so he has commissioned a new set of PowerShell scripting challenges. 
+These tasks are aimed at beginner level PowerShell scripters, with a few bonuses targeted toward intermediate users. Although everyone is welcome and encouraged to try their hand.
+
+----
+### The Challenges
+
+You need to create simple PowerShell functions to meet the following goals. Each task should result in a separate function.
+
+		Given 2 lengths of a right triangle, calculate the missing length. You will need to let the user specify which sides (traditionally A,B and C) of the triangle they are specifying.
+		Calculate the area of a circle with a given diameter.
+		Calculate the volume of a sphere with a given diameter.
+		Calculate the volume of a cylinder from a given diameter and height.
+		Calculate the factorial of an integer.
+
+----
+### Intermediate Additions
+
+For those of you with a bit more PowerShell experience, your functions should include parameter validation and error handling. Your function should have an option to write a rich object to the pipeline. 
+For example, the first challenge might have you write an object with properties of A, B, and C. Finally, you should be able to create a simple module with a manifest.
---
+++ Back_to_school/back2school.ps1
@@ -0,0 +1,143 @@
+
+function menu {
+    do{
+        $fail = 0
+        $selection = Read-Host "Select option :
+        1. Side of a right triangle.
+        2. Area of a circle.
+        3. Volume of a sphere.
+        4. Volume of a cylinder.
+        5. Factorial of an integer.
+        "
+        switch ($selection) {
+            1 {
+                do{
+                    $fail = 0
+                    $selection = Read-Host "What do you want to calculate ?
+                    1. The hypotenuse.
+                    2. One of the sides.
+                    "
+                    switch ($selection) {
+                        1 { Triangle 1 }
+                        2 { Triangle 2 }
+                        Default {"Invalid Input"; $fail = 1}
+                    }
+                } until ($fail -eq 0)
+            }
+            2 { circle }
+
+            3 { sphere }
+
+            4 { cylinder }
+
+            5 { factorial }
+
+            Default{$fail = 1}
+
+        }
+    }until($fail -eq 0)
+}
+Function Triangle (){
+    [CmdletBinding()]
+    Param(
+        [parameter(ValueFromPipeline)]
+        [ValidateNotNullOrEmpty()]
+        [int]$selection
+    )
+
+    process{
+        do{
+            try{
+            $fail = 0
+            [ValidateRange(1,10000)][double]$a = Read-Host "Enter first side length"
+            } catch{"Invalid length"; $fail = 1}
+        } until ($fail -eq 0)
+
+        if ($selection -eq 1){
+            do{
+              try{
+                $fail = 0
+                [ValidateRange(1,10000)][double]$b = Read-Host "Enter second side length"
+                } catch{"Invalid length"; $fail = 1}
+            } until ($fail -eq 0)
+
+            [math]::Sqrt(($a * $a) + ($b * $b))
+        }
+        elseif($selection -eq 2){
+            do{
+                try{
+                  $fail = 0
+                  [ValidateRange(1,10000)][double]$b = Read-Host "Enter hypotenuse length"
+                  } catch{"Invalid length"; $fail = 1}
+              } until (($fail -eq 0) -and ($b -gt $a ))
+
+              [math]::Sqrt(($b * $b) - ($a * $a))
+        }
+    }
+}
+
+function circle (){
+
+    process{
+        do{
+            try{
+            $fail = 0
+            [ValidateRange(1,10000)][double]$a = Read-Host "Enter diameter"
+            } catch{"Invalid diameter"; $fail = 1}
+        } until ($fail -eq 0)
+        ([math]::PI * ([math]::Pow(($a / 2), 2)))
+    }
+
+
+}
+
+function sphere(){
+
+    process{
+        do{
+            try{
+            $fail = 0
+            [ValidateRange(1,10000)][double]$a = Read-Host "Enter diameter"
+            } catch{"Invalid diameter"; $fail = 1}
+        } until ($fail -eq 0)
+      (([math]::PI * ([math]::Pow($a, 3))) / 6)
+    }
+}
+
+
+function cylinder(){
+    process{
+        do{
+            try{
+            $fail = 0
+            [ValidateRange(1,10000)][double]$a = Read-Host "Enter diameter"
+            } catch{"Invalid diameter"; $fail = 1}
+        } until ($fail -eq 0)
+
+        do{
+            try{
+            $fail = 0
+            [ValidateRange(1,10000)][double]$b = Read-Host "Enter height"
+            } catch{"Invalid height"; $fail = 1}
+        } until ($fail -eq 0)
+
+        ([math]::PI * ([math]::Pow(($a / 2), 2) * $b))
+
+    }
+}
+function factorial(){
+
+    do{
+        try{
+        $fail = 0
+        [ValidateRange(1,10000)][int]$a = Read-Host "Enter value"
+        } catch{"Invalid value"; $fail = 1}
+    } until ($fail -eq 0)
+    $res = $a
+    for ($i = 1; $i -lt $a; $i++){
+    $res = ($res * ($a - $i))
+    }
+    Write-Output $res
+}
+
+menu
